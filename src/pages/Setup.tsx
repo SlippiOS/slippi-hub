fa0107bb9d292b42832be15ccc808a4ed370e189
Ryan Wendell <ryanjwendell@gmail.com>
2025-05-27 13:35:32 -0500

    feat: add onboarding

--- src/pages/Setup.tsx
+++ src/pages/Setup.tsx
@@ -1,25 +1,35 @@
 /*	We want to:
- *		1. Programmatically check if the user has slippi installed and set the dolphin executable location (maybe read from slippi-launcher datastore?)
+ *      //TODO flesh out
+ *		X 1. Programmatically check if the user has slippi installed and set the dolphin executable location (maybe read from slippi-launcher datastore?)
  *			a. see https://gemini.google.com/app/acb86efee2bdcb9a
  *			b. as a last resort -- ask user if they want to install or locate their own
  *			c. after this we should ask for replays folder
- *		2. Ask the user to select a default melee ISO
- *		3. Ask if they have any other melee ISOs (and to select that folder)
- *		4. Finally -- set isCreated to true
+ *		X 2. Ask the user to select a default melee ISO -- grabbed from slippi settings
+ *		  3. Ask if they have any other melee ISOs (and to select that folder) //TODO
+ *		X 4. Finally -- set isCreated to true -- commented out for now.  will uncomment when tested
  */
 
 import { createResource } from "solid-js";
-import { findSlippiDolphin } from "../utils/setupUtils";
+import { findSlippiDetails } from "../utils/setupUtils";
 import { LazyStore } from "@tauri-apps/plugin-store";
 
 export default function Setup() {
   const initStore = new LazyStore("init.json");
-  const [slippiExecutable] = createResource(findSlippiDolphin);
+  const [slippiDetails] = createResource(findSlippiDetails, {
+    onHydrated: () => {
+      initStore.set("dolphinPath", slippiDetails()!.dolphinPath);
+      initStore.set("meleeIso", slippiDetails()!.meleeIso);
+      // initStore.set("isCreated", true);
+    },
+  });
 
   return (
     <>
       <h1>SETUP</h1>
-      <span>{slippiExecutable.loading && "Loading..."}</span>
+      {console.log(initStore.entries())}
+      <span>{slippiDetails.loading && "Loading..."}</span>
+      <p>Found executable at "{slippiDetails()?.dolphinPath}"</p>
+      <p>Found melee iso at "{slippiDetails()?.meleeIso}"</p>
     </>
   );
 }
--- src/utils/setupUtils.ts
+++ src/utils/setupUtils.ts
@@ -1,12 +1,40 @@
 import { useNavigate } from "@solidjs/router";
+import { BaseDirectory, readTextFile } from "@tauri-apps/plugin-fs";
 import { LazyStore } from "@tauri-apps/plugin-store";
 
+type slippiLauncherSettings = {
+  settings: {
+    isoPath: string;
+    useNetplayBeta: boolean;
+  };
+};
+
+type slippiDetails = {
+  dolphinPath: string;
+  meleeIso: string;
+};
+
 export async function checkSetup() {
-	const navigate = useNavigate();
-	const initStore = new LazyStore("init.json");
-	return await initStore.has('isCreated') ? navigate('/home') : navigate('/setup');
+  const navigate = useNavigate();
+  const initStore = new LazyStore("init.json");
+  return (await initStore.has("isCreated"))
+    ? navigate("/home")
+    : navigate("/setup");
 }
 
-export function findSlippiDolphin() {
+export async function findSlippiDetails(): Promise<slippiDetails> {
+  const NETPLAY_BETA_EXECUTABLE =
+    "./Slippi Launcher/netplay-beta/Slippi_Netplay_Mainline-x86_64.AppImage";
+  const NETPLAY_EXECUTABLE =
+    "./Slippi Launcher/netplay/Slippi_Online-x86_64.AppImage";
 
+  const { settings }: slippiLauncherSettings = JSON.parse(
+    await readTextFile("Slippi\ Launcher/Settings", {
+      baseDir: BaseDirectory.Config,
+    }),
+  );
+
+  return settings.useNetplayBeta
+    ? { dolphinPath: NETPLAY_BETA_EXECUTABLE, meleeIso: settings.isoPath }
+    : { dolphinPath: NETPLAY_EXECUTABLE, meleeIso: settings.isoPath };
 }
